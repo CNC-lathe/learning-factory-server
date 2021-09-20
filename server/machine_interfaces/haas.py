901b5f07772688c3c13351c522a251ae223aefe6
lminton52 <lminton52@vt.edu>
2021-09-20 15:48:31 -0400

    minor tweaks from LF testing

--- server/machine_interfaces/haas.py
+++ server/machine_interfaces/haas.py
@@ -1,6 +1,9 @@
 # Luke Minton, MDE Fall 2021, VT ISE Learning Factory, Interface from Haas CNC machine to LF server
 # Machine currently has a serial (RS232) to USB port going into a Raspberry Pi (Canakit): Assuming this was used for previous data collection
-# TO DO: Test in LF by directly plugging into Haas and then debug
+# DONE: Test in LF by directly plugging into Haas and then debug
+# RESULT: Retrieved data from the Haas over serial to USB cable, I do not have the qualifications to run an actual job on the Haas but data
+# reception was verified by seeing a value returned for the coolant level and monitoring the sending of macros and recieving of data through
+# free software called Serial Port Monitor
 
 import socket, serial, time, sys
 from base import MachineInterface
@@ -34,11 +37,13 @@ class HaasInterface(MachineInterface):
             try:
                 ser.write(constant)
                 while True:
-                    output = ser.readline().decode("utf-8").strip()
+                    output = ser.readline().decode("utf-8")
                     if len(output) > 4:
                         break
+                output = output.split(",")[2].strip()[0:8]
             except:
                 output = '0'
+            return output
 
         # Helper function to create a socket
         def socket_init():
@@ -91,54 +96,66 @@ class HaasInterface(MachineInterface):
             try:
                 # Coolant Level Data
                 coolant_lvl = ''
-                data_dict['Coolant Level'] = get_data(serial_port, COOLANT_LEVEL, coolant_lvl)
+                coolant_lvl = get_data(serial_port, COOLANT_LEVEL, coolant_lvl)
+                data_dict['Coolant Level'] = coolant_lvl
                 print("Coolant level: " + coolant_lvl)
 
                 # Spindle Speed RPM
                 spindle_spd = ''
-                data_dict['Spindle Speed'] = get_data(serial_port, SPINDLE_SPEED, spindle_spd)
+                spindle_spd = get_data(serial_port, SPINDLE_SPEED, spindle_spd)
+                data_dict['Spindle Speed'] = spindle_spd
                 print("Spindle speed: " + spindle_spd)
 
                 # Machine Coords (X, Y, Z, A, B)
                 machine_x = ''
-                data_dict['Machine Coordinate X Value'] = get_data(serial_port, MACHINE_COORD_X, machine_x)
+                machine_x = get_data(serial_port, MACHINE_COORD_X, machine_x)
+                data_dict['Machine Coordinate X Value'] = machine_x
                 print("Machine coordinate X: " + machine_x)
 
                 machine_y = ''
-                data_dict['Machine Coordinate Y Value'] = get_data(serial_port, MACHINE_COORD_Y, machine_y)
+                machine_y = get_data(serial_port, MACHINE_COORD_Y, machine_y)
+                data_dict['Machine Coordinate Y Value'] = machine_y
                 print("Machine coordinate Y: " + machine_y)
 
                 machine_z = ''
-                data_dict['Machine Coordinate Z Value'] = get_data(serial_port, MACHINE_COORD_Z, machine_z)
+                machine_z = get_data(serial_port, MACHINE_COORD_Z, machine_z)
+                data_dict['Machine Coordinate Z Value'] = machine_z
                 print("Machine coordinate Z: " + machine_z)
 
                 machine_a = ''
-                data_dict['Machine Coordinate A Value'] = get_data(serial_port, MACHINE_COORD_A, machine_a)
+                machine_a = get_data(serial_port, MACHINE_COORD_A, machine_a)
+                data_dict['Machine Coordinate A Value'] = machine_a
                 print("Machine coordinate A: " + machine_a)
 
                 machine_b = ''
-                data_dict['Machine Coordinate B Value'] = get_data(serial_port, MACHINE_COORD_B, machine_b)
+                machine_b = get_data(serial_port, MACHINE_COORD_B, machine_b)
+                data_dict['Machine Coordinate B Value'] = machine_b
                 print("Machine coodinate B: " + machine_b)
 
                 # Work Coords (X, Y, Z, A, B)
                 work_x = ''
-                data_dict['Work Coordinate X Value'] = get_data(serial_port, WORK_COORD_X, work_x)
+                work_x = get_data(serial_port, WORK_COORD_X, work_x)
+                data_dict['Work Coordinate X Value'] = work_x
                 print("Work coordinate X: " + work_x)
 
                 work_y = ''
-                data_dict['Work Coordinate Y Value'] = get_data(serial_port, WORK_COORD_Y, work_y)
+                work_y = get_data(serial_port, WORK_COORD_Y, work_y)
+                data_dict['Work Coordinate Y Value'] = work_y
                 print("Work coordinate Y: " + work_y)
 
                 work_z = ''
-                data_dict['Work Coordinate Z Value'] = get_data(serial_port, WORK_COORD_Z, work_z)
+                work_z = get_data(serial_port, WORK_COORD_Z, work_z)
+                data_dict['Work Coordinate Z Value'] = work_z
                 print("Work coordinate Z: " + work_z)
 
                 work_a = ''
-                data_dict['Work Coordinate A Value'] = get_data(serial_port, WORK_COORD_A, work_a)
+                work_a = get_data(serial_port, WORK_COORD_A, work_a)
+                data_dict['Work Coordinate A Value'] = work_a
                 print("Work coordinate A: " + work_a)
 
                 work_b = ''
-                data_dict['Work Coordinate B Value'] = get_data(serial_port, WORK_COORD_B, work_b)
+                work_b = get_data(serial_port, WORK_COORD_B, work_b)
+                data_dict['Work Coordinate B Value'] = work_b
                 print("Work coordinate B: " + work_b)
 
             except Exception as ex:
