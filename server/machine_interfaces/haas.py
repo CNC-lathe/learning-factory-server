53f53eaec3ef26aa05fd1bd17371ef175bca6d87
lminton52 <lminton52@vt.edu>
2021-09-26 19:09:22 -0400

    converted from script to match base

--- server/machine_interfaces/haas.py
+++ server/machine_interfaces/haas.py
@@ -1,12 +1,11 @@
 # Luke Minton, MDE Fall 2021, VT ISE Learning Factory, Interface from Haas CNC machine to LF server
-# Machine currently has a serial (RS232) to USB port going into a Raspberry Pi (Canakit): Assuming this was used for previous data collection
-# DONE: Test in LF by directly plugging into Haas and then debug
-# RESULT: Retrieved data from the Haas over serial to USB cable, I do not have the qualifications to run an actual job on the Haas but data
-# reception was verified by seeing a value returned for the coolant level and monitoring the sending of macros and recieving of data through
-# free software called Serial Port Monitor
+# Machine currently has a serial (RS232) to USB port
 
-import socket, serial, time, sys
+import serial, time, sys, zmq, pickle
+from typing import Dict, Any
+from threading import Thread
 from base import MachineInterface
+from lf_utils import retry
 
 # Haas macros for pulling data
 COOLANT_LEVEL = b"?Q600 1094\r\n"
@@ -22,15 +21,70 @@ WORK_COORD_Z = b"?Q600 5043\r\n"
 WORK_COORD_A = b"?Q600 5044\r\n"
 WORK_COORD_B = b"?Q600 5045\r\n"
 
-# Network macros
-HOST = 'localhost'
-PORT = 7878
-
 # Inherit from the Machine Interface class defined in base.py
 class HaasInterface(MachineInterface):
-            
+    
+    def __init__(self, publish_port: int, machine_config: Dict, serial_port: str):
+
+        # set machine config
+        self._machine_config = machine_config
+
+        # set serial port name (i.e. COM3)
+        self._serial_port = serial.Serial(port = serial_port, baudrate = 9600, bytesize = serial.SEVENBITS, xonxoff = True, timeout = 5)
+
+        # Attempt to open a serial port for communication with the Haas
+        try:
+            self._serial_port.open()
+        except serial.SerialException:
+            if self._serial_port.is_open:
+                try:
+                    print("Port was open, attempting to close and reopen")
+                    self._serial_port.close()
+                    time.sleep(2)
+                    self._serial_port.open()
+                except:
+                    print("Failed to close port")
+                    event.clear()
+            else:
+                print("Failed to connect to serial port, either in use or does not exist")
+                event.clear() 
+
+        # set machine name
+        self._machine_name = self._machine_config["Haas CNC"]
+
+        # set up publish address
+        self._publish_address = f"tcp://127.0.0.1:{publish_port}"
+
+        # set up publish socket
+        self._context = zmq.Context()
+        self._publish_socket = self._context.socket(zmq.PUB)
+        retry(
+            self._publish_socket.connect,
+            self._publish_address,
+            handled_exceptions=zmq.error.ZMQError
+        )
+
+        # initialize thread
+        Thread.__init__(self)
+        self._stopped = False
+
+    def run(self):
+        # loop until thread is stopped
+        while not self._stopped:
+            # get machine data
+            machine_data = self._poll_machine()
+
+            # publish machine data
+            self._publish_data(self._publish_socket, self._machine_name, machine_data)
+
+        # close ZMQ socket and context
+        self._close_zmq()
+
+    def stop(self):
+        self._stopped = True
+
     # Redefine abstract method to poll a machine for data
-    def _poll_machine():
+    def _poll_machine(self):
 
         # Helper function to get write macros to Haas requesting specific data
         def get_data(ser, constant, output):
@@ -45,128 +99,90 @@ class HaasInterface(MachineInterface):
                 output = '0'
             return output
 
-        # Helper function to create a socket
-        def socket_init():
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            except socket.error as err:
-                print("Failed to create socket, Error: " + str(err[0] + " Message " + msg[1]))
-                sys.exit()
-            
-            try:
-                s.bind((HOST, PORT))
-            except socket.error as err:
-                print("Failed to bind, Error: " + str(err[0] + " Message " + msg[1]))
-                sys.exit()
-            
-            s.listen(5)
+        print("Begin data collection:")
 
-        # Open a socket for Haas socket communication
-        socket_init()
-
-        # Define serial port for Haas data communication (/dev/pts/0 represents a virtual serial port used for testing)
-        # Direct USB to serial connection would use a COM port such as COM11
-        serial_port = serial.Serial(port = "/dev/pts/0", baudrate = 9600, bytesize = serial.SEVENBITS, xonxoff = True, timeout = 5)
-
-        # Attempt to open a serial port for communication with the Haas
-        try:
-            serial_port.open()
-        except serial.SerialException:
-            if serial_port.is_open:
-                try:
-                    print("Port was open, attempting to close and reopen")
-                    serial_port.close()
-                    time.sleep(2)
-                    serial_port.open()
-                except:
-                    print("Failed to close port")
-                    event.clear()
-            else:
-                print("Failed to connect to serial port, either in use or does not exist")
-                event.clear() 
-        
-        # Error checking statement
-        print("CHECK")
-
-        # Define a dictionary for data housing
-        data_dict = {}
-
-        # Begin fetching data from Haas
+        # Begin fetching data from Haas (comment out while loop for one-time testing)
         while True:
             try:
                 # Coolant Level Data
                 coolant_lvl = ''
-                coolant_lvl = get_data(serial_port, COOLANT_LEVEL, coolant_lvl)
-                data_dict['Coolant Level'] = coolant_lvl
+                coolant_lvl = get_data(self._serial_port, COOLANT_LEVEL, coolant_lvl)
+                self._machine_config['Coolant Level'] = coolant_lvl
                 print("Coolant level: " + coolant_lvl)
 
                 # Spindle Speed RPM
                 spindle_spd = ''
-                spindle_spd = get_data(serial_port, SPINDLE_SPEED, spindle_spd)
-                data_dict['Spindle Speed'] = spindle_spd
+                spindle_spd = get_data(self._serial_port, SPINDLE_SPEED, spindle_spd)
+                self._machine_config['Spindle Speed'] = spindle_spd
                 print("Spindle speed: " + spindle_spd)
 
                 # Machine Coords (X, Y, Z, A, B)
                 machine_x = ''
-                machine_x = get_data(serial_port, MACHINE_COORD_X, machine_x)
-                data_dict['Machine Coordinate X Value'] = machine_x
+                machine_x = get_data(self._serial_port, MACHINE_COORD_X, machine_x)
+                self._machine_config['Machine Coordinate X Value'] = machine_x
                 print("Machine coordinate X: " + machine_x)
 
                 machine_y = ''
-                machine_y = get_data(serial_port, MACHINE_COORD_Y, machine_y)
-                data_dict['Machine Coordinate Y Value'] = machine_y
+                machine_y = get_data(self._serial_port, MACHINE_COORD_Y, machine_y)
+                self._machine_config['Machine Coordinate Y Value'] = machine_y
                 print("Machine coordinate Y: " + machine_y)
 
                 machine_z = ''
-                machine_z = get_data(serial_port, MACHINE_COORD_Z, machine_z)
-                data_dict['Machine Coordinate Z Value'] = machine_z
+                machine_z = get_data(self._serial_port, MACHINE_COORD_Z, machine_z)
+                self._machine_config['Machine Coordinate Z Value'] = machine_z
                 print("Machine coordinate Z: " + machine_z)
 
                 machine_a = ''
-                machine_a = get_data(serial_port, MACHINE_COORD_A, machine_a)
-                data_dict['Machine Coordinate A Value'] = machine_a
+                machine_a = get_data(self._serial_port, MACHINE_COORD_A, machine_a)
+                self._machine_config['Machine Coordinate A Value'] = machine_a
                 print("Machine coordinate A: " + machine_a)
 
                 machine_b = ''
-                machine_b = get_data(serial_port, MACHINE_COORD_B, machine_b)
-                data_dict['Machine Coordinate B Value'] = machine_b
+                machine_b = get_data(self._serial_port, MACHINE_COORD_B, machine_b)
+                self._machine_config['Machine Coordinate B Value'] = machine_b
                 print("Machine coodinate B: " + machine_b)
 
                 # Work Coords (X, Y, Z, A, B)
                 work_x = ''
-                work_x = get_data(serial_port, WORK_COORD_X, work_x)
-                data_dict['Work Coordinate X Value'] = work_x
+                work_x = get_data(self._serial_port, WORK_COORD_X, work_x)
+                self._machine_config['Work Coordinate X Value'] = work_x
                 print("Work coordinate X: " + work_x)
 
                 work_y = ''
-                work_y = get_data(serial_port, WORK_COORD_Y, work_y)
-                data_dict['Work Coordinate Y Value'] = work_y
+                work_y = get_data(self._serial_port, WORK_COORD_Y, work_y)
+                self._machine_config['Work Coordinate Y Value'] = work_y
                 print("Work coordinate Y: " + work_y)
 
                 work_z = ''
-                work_z = get_data(serial_port, WORK_COORD_Z, work_z)
-                data_dict['Work Coordinate Z Value'] = work_z
+                work_z = get_data(self._serial_port, WORK_COORD_Z, work_z)
+                self._machine_config['Work Coordinate Z Value'] = work_z
                 print("Work coordinate Z: " + work_z)
 
                 work_a = ''
-                work_a = get_data(serial_port, WORK_COORD_A, work_a)
-                data_dict['Work Coordinate A Value'] = work_a
+                work_a = get_data(self._serial_port, WORK_COORD_A, work_a)
+                self._machine_config['Work Coordinate A Value'] = work_a
                 print("Work coordinate A: " + work_a)
 
                 work_b = ''
-                work_b = get_data(serial_port, WORK_COORD_B, work_b)
-                data_dict['Work Coordinate B Value'] = work_b
+                work_b = get_data(self._serial_port, WORK_COORD_B, work_b)
+                self._machine_config['Work Coordinate B Value'] = work_b
                 print("Work coordinate B: " + work_b)
 
             except Exception as ex:
                 print("Failed to retrive any data from machine" + str(ex))
                 time.sleep(2)
     
-        serial_port.close()
+        self._serial_port.close()
 
         # Return the dictionary of data
-        return data_dict
+        return self._machine_config
+    
+    @staticmethod
+    def _publish_data(publish_socket: zmq.Socket, machine_name: str, machine_data: Dict[str, Any]):
+        
+        publish_socket.send_multipart([bytes(machine_name, "utf-8"), pickle.dumps(machine_data)])
 
-# Used to directly call the poll machine method for testing
-HaasInterface._poll_machine()
+    def _close_zmq(self):
+        # destroy context
+        self._context.destroy()
         
