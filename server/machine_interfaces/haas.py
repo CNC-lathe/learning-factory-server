cd5cdf30bf11c5d3a84990bf21a644786e27f79e
lminton52 <lminton52@vt.edu>
2021-09-27 01:38:55 -0400

    small changes

--- server/machine_interfaces/haas.py
+++ server/machine_interfaces/haas.py
@@ -90,9 +90,14 @@ class HaasInterface(MachineInterface):
         def get_data(ser, constant, output):
             try:
                 ser.write(constant)
+                start_timer = time.time() # start a timer
                 while True:
                     output = ser.readline().decode("utf-8")
                     if len(output) > 4:
+                        break
+                    delta = time.time() - start_timer
+                    if delta >= 3: # wait 3 seconds to see if data arrives
+                        output = '0' # no data recieved
                         break
                 output = output.split(",")[2].strip()[0:8]
             except:
@@ -101,76 +106,75 @@ class HaasInterface(MachineInterface):
 
         print("Begin data collection:")
 
-        # Begin fetching data from Haas (comment out while loop for one-time testing)
-        while True:
-            try:
-                # Coolant Level Data
-                coolant_lvl = ''
-                coolant_lvl = get_data(self._serial_port, COOLANT_LEVEL, coolant_lvl)
-                self._machine_config['Coolant Level'] = coolant_lvl
-                print("Coolant level: " + coolant_lvl)
+        # Begin fetching data from Haas
+        try:
+            # Coolant Level Data
+            coolant_lvl = ''
+            coolant_lvl = get_data(self._serial_port, COOLANT_LEVEL, coolant_lvl)
+            self._machine_config['Coolant Level'] = coolant_lvl
+            print("Coolant level: " + coolant_lvl)
 
-                # Spindle Speed RPM
-                spindle_spd = ''
-                spindle_spd = get_data(self._serial_port, SPINDLE_SPEED, spindle_spd)
-                self._machine_config['Spindle Speed'] = spindle_spd
-                print("Spindle speed: " + spindle_spd)
+            # Spindle Speed RPM
+            spindle_spd = ''
+            spindle_spd = get_data(self._serial_port, SPINDLE_SPEED, spindle_spd)
+            self._machine_config['Spindle Speed'] = spindle_spd
+            print("Spindle speed: " + spindle_spd)
 
-                # Machine Coords (X, Y, Z, A, B)
-                machine_x = ''
-                machine_x = get_data(self._serial_port, MACHINE_COORD_X, machine_x)
-                self._machine_config['Machine Coordinate X Value'] = machine_x
-                print("Machine coordinate X: " + machine_x)
+            # Machine Coords (X, Y, Z, A, B)
+            machine_x = ''
+            machine_x = get_data(self._serial_port, MACHINE_COORD_X, machine_x)
+            self._machine_config['Machine Coordinate X Value'] = machine_x
+            print("Machine coordinate X: " + machine_x)
 
-                machine_y = ''
-                machine_y = get_data(self._serial_port, MACHINE_COORD_Y, machine_y)
-                self._machine_config['Machine Coordinate Y Value'] = machine_y
-                print("Machine coordinate Y: " + machine_y)
+            machine_y = ''
+            machine_y = get_data(self._serial_port, MACHINE_COORD_Y, machine_y)
+            self._machine_config['Machine Coordinate Y Value'] = machine_y
+            print("Machine coordinate Y: " + machine_y)
 
-                machine_z = ''
-                machine_z = get_data(self._serial_port, MACHINE_COORD_Z, machine_z)
-                self._machine_config['Machine Coordinate Z Value'] = machine_z
-                print("Machine coordinate Z: " + machine_z)
+            machine_z = ''
+            machine_z = get_data(self._serial_port, MACHINE_COORD_Z, machine_z)
+            self._machine_config['Machine Coordinate Z Value'] = machine_z
+            print("Machine coordinate Z: " + machine_z)
 
-                machine_a = ''
-                machine_a = get_data(self._serial_port, MACHINE_COORD_A, machine_a)
-                self._machine_config['Machine Coordinate A Value'] = machine_a
-                print("Machine coordinate A: " + machine_a)
+            machine_a = ''
+            machine_a = get_data(self._serial_port, MACHINE_COORD_A, machine_a)
+            self._machine_config['Machine Coordinate A Value'] = machine_a
+            print("Machine coordinate A: " + machine_a)
 
-                machine_b = ''
-                machine_b = get_data(self._serial_port, MACHINE_COORD_B, machine_b)
-                self._machine_config['Machine Coordinate B Value'] = machine_b
-                print("Machine coodinate B: " + machine_b)
+            machine_b = ''
+            machine_b = get_data(self._serial_port, MACHINE_COORD_B, machine_b)
+            self._machine_config['Machine Coordinate B Value'] = machine_b
+            print("Machine coodinate B: " + machine_b)
 
-                # Work Coords (X, Y, Z, A, B)
-                work_x = ''
-                work_x = get_data(self._serial_port, WORK_COORD_X, work_x)
-                self._machine_config['Work Coordinate X Value'] = work_x
-                print("Work coordinate X: " + work_x)
+            # Work Coords (X, Y, Z, A, B)
+            work_x = ''
+            work_x = get_data(self._serial_port, WORK_COORD_X, work_x)
+            self._machine_config['Work Coordinate X Value'] = work_x
+            print("Work coordinate X: " + work_x)
 
-                work_y = ''
-                work_y = get_data(self._serial_port, WORK_COORD_Y, work_y)
-                self._machine_config['Work Coordinate Y Value'] = work_y
-                print("Work coordinate Y: " + work_y)
+            work_y = ''
+            work_y = get_data(self._serial_port, WORK_COORD_Y, work_y)
+            self._machine_config['Work Coordinate Y Value'] = work_y
+            print("Work coordinate Y: " + work_y)
 
-                work_z = ''
-                work_z = get_data(self._serial_port, WORK_COORD_Z, work_z)
-                self._machine_config['Work Coordinate Z Value'] = work_z
-                print("Work coordinate Z: " + work_z)
+            work_z = ''
+            work_z = get_data(self._serial_port, WORK_COORD_Z, work_z)
+            self._machine_config['Work Coordinate Z Value'] = work_z
+            print("Work coordinate Z: " + work_z)
 
-                work_a = ''
-                work_a = get_data(self._serial_port, WORK_COORD_A, work_a)
-                self._machine_config['Work Coordinate A Value'] = work_a
-                print("Work coordinate A: " + work_a)
+            work_a = ''
+            work_a = get_data(self._serial_port, WORK_COORD_A, work_a)
+            self._machine_config['Work Coordinate A Value'] = work_a
+            print("Work coordinate A: " + work_a)
 
-                work_b = ''
-                work_b = get_data(self._serial_port, WORK_COORD_B, work_b)
-                self._machine_config['Work Coordinate B Value'] = work_b
-                print("Work coordinate B: " + work_b)
+            work_b = ''
+            work_b = get_data(self._serial_port, WORK_COORD_B, work_b)
+            self._machine_config['Work Coordinate B Value'] = work_b
+            print("Work coordinate B: " + work_b)
 
-            except Exception as ex:
-                print("Failed to retrive any data from machine" + str(ex))
-                time.sleep(2)
+        except Exception as ex:
+            print("Failed to retrive any data from machine" + str(ex))
+            time.sleep(2)
     
         self._serial_port.close()
 
